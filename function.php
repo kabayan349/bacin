997f40d043a2e4ad684c6915bdca7d98cbe07d27
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-23 02:13:48 +0700

    gd

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ function request($url, $token = null, $data = null, $pin = null, $otpsetpin = nu
     $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
     $header[] = "Connection: keep-alive";
     $header[] = "X-User-Locale: id_ID";
-    $header[] = "X-Location: -8.669354,115.220129";
+    $header[] = "X-Location: -5.140848,119.483250";
     $header[] = "X-Location-Accuracy: 3.0";
     if ($pin):
     $header[] = "pin: $pin";
